29ba756dbc6b2ef6cbebb385c6b2c1cc3b23d668
Mohammad Jawwad <mjawwad@stuy.edu>
2019-03-10 20:56:43 -0400

    added skeleton file for partition hw

---
+++ partition.java
@@ -0,0 +1,13 @@
+public class partition{
+    /*Modify the array such that:
+   *1. Only the indices from start to end inclusive are considered in range
+   *2. A random index from start to end inclusive is chosen, the corresponding
+   *   element is designated the pivot element.
+   *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
+   *4. all elements in range that are larger than the pivot element are placed after the pivot element.
+   *@return the index of the final position of the pivot element.
+   */
+  public int partition ( int [] data, int start, int end){
+    return 1;
+  }
+}
