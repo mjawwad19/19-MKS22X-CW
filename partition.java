a999e2c1d9a154b1e549292168a9049cf018894b
Mohammad Jawwad <mjawwad@stuy.edu>
2019-03-10 21:05:02 -0400

    changed typing for testing to static void, partition currently swaps a random index in data to the front

--- partition.java
+++ partition.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class partition{
     /*Modify the array such that:
    *1. Only the indices from start to end inclusive are considered in range
@@ -7,7 +8,19 @@ public class partition{
    *4. all elements in range that are larger than the pivot element are placed after the pivot element.
    *@return the index of the final position of the pivot element.
    */
-  public int partition ( int [] data, int start, int end){
-    return 1;
+  public static void partition ( int [] data, int start, int end){
+    int index = (int) (Math.random() * (end - start + 1) + start);
+    // choose a random index that is not start nor end (or attempt not to, as discussed in class)
+    int pivot = data[index];
+    //pivot is what we will use the separate the data that is less or greater. We temporarily store since swapping is involved for constant time!
+    data[index] = data[0];
+    data[0] = pivot;
+  }
+
+  public static void main(String[] args) {
+    int[] data = {8, 6, 7, 5, 3, 0, 9};
+    partition(data, 0, data.length -1);
+    System.out.println(Arrays.toString(data));
+    //7 should be at the front right now and 8 in 7's place;
   }
 }
